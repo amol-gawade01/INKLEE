3e4eecafff706041f6e24c6289fb46104924f747
Amol <amolgawade56@gmail.com>
2025-01-04 23:53:20 +0530

    Add Profile UI

--- src/component/Profile.jsx
+++ src/component/Profile.jsx
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import DBservice from "../appwrite/config";
 import PostCard from "./PostCard";
+import user from "../../src/assets/Images/user.png";
 
 function Profile() {
   const userData = useSelector((store) => store.auth.userData);
   const [userPosts, setUserPosts] = useState([]);
-  const [loading, setLoading] = useState(true); // State to track loading status
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchUserPosts = async () => {
@@ -23,7 +24,7 @@ function Profile() {
       } catch (error) {
         console.log("Error while getting user posts from backend", error);
       } finally {
-        setLoading(false); // Set loading to false once data is fetched
+        setLoading(false);
       }
     };
 
@@ -31,28 +32,35 @@ function Profile() {
   }, [userData]);
 
   return (
-    <div className="w-full m-auto flex flex-col">
-      <div className="w-56 bg-gray-300 text-black m-4 ml-8">
-        <h3>Username: {userData?.name}</h3>
-        <h4>Blogs: {userPosts?.length || 0}</h4>
+    <div className="w-full m-auto mt-16 overflow-hidden">
+      <div className="w-1/3 m-0 flex items-center mb-24 ml-24">
+        <div>
+          <img src={user} className="w-42 h-40" alt="User" />
+        </div>
+        <div className="w-56 text-black m-4 ml-20">
+          <h3 className="font-semibold text-2xl">Name: {userData?.name}</h3>
+          <h3 className="font-semibold text-gray-600 text-xl">
+            Blogs: {userPosts?.length || 0}
+          </h3>
+        </div>
       </div>
+
       {loading ? (
-        <h3>Loading...</h3> // Show loading while fetching data
+        <h3>Loading...</h3>
       ) : userPosts.length === 0 ? (
-        <h3>No posts available</h3> // Show if no posts found
+        <h3>No posts available</h3>
       ) : (
-        <div className="w-[25%] min-h-screen m-4 bg-gray flex">
-          <div className="w-auto">
-            {userPosts.map((post) => (
-              <PostCard
-                key={post.$id}
-                $id={post.$id}
-                title={post.title}
-                featuredImage={post.featuredImage}
-                likes={post.likes}
-              />
-            ))}
-          </div>
+        <div className="min-h-screen m-4 bg-gray flex flex-row flex-wrap gap-4 ml-14">
+          {userPosts.map((post) => (
+            <PostCard
+              key={post.$id}
+              $id={post.$id}
+              title={post.title}
+              featuredImage={post.featuredImage}
+              likes={post.likes}
+              userId={post.userId}
+            />
+          ))}
         </div>
       )}
     </div>
